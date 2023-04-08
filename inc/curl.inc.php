157d7d285da9bc6064c69605b9863685318146aa
Charith <charithakalanka@live.com>
2023-04-08 10:20:52 +1000

    Post to API and retrieve completion

---
+++ inc/curl.inc.php
@@ -0,0 +1,64 @@
+<?php
+
+include 'session.inc.php';
+
+$apiKey = "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";
+// print_r($_POST);
+
+if (!strlen($_POST['irp'])) {
+  // No content to post to API
+  header( 'location: /index.php?error=1' );
+  exit();
+}
+
+$_SESSION["irp"] = $_POST['irp'];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+$post = [
+  "model" => "text-davinci-003",
+  "prompt" => "summerise the following playbook into a list of platform independent objectives\n\n".$_POST['irp']."\n\nthe objectives are:",
+  "temperature" => 0,
+  "max_tokens" => 500,
+  "top_p" => 1,
+  "frequency_penalty" => 0,
+  "presence_penalty" => 0
+];
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+$headers[] = 'Authorization: Bearer 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';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+
+$result = json_decode($result, true);
+// print_r($result);
+
+
+if (array_key_exists("error", $result)) {
+  // OpenAI API returned an error
+  echo "API Error<br>";
+  print_r($result);
+
+} else {
+  echo "GOOD<br>";
+  // print_r($result["choices"][0]["text"]);
+  $_SESSION["obj"] = $result["choices"][0]["text"];
+
+  print_r($_SESSION);
+
+  // Reload the home page
+  header( 'location: /index.php' );
+  exit();
+}
+
+?>
--- index.php
+++ index.php
@@ -1,21 +1,40 @@
 <?php
+  include 'inc/session.inc.php';
   include 'inc/header.inc.php';
 ?>
-  <h1>IR GPT</h1>
+  <a href="/index.php"><h1>IR GPT</h1></a>
   <p>This is a Incdent Response bot built using the GPT-3 LLM</p>
+  <?php
+    if (isset($_GET['error'])) {
+  ?>
+  <section class="error">
+    <?php if ($_GET['error']==1) { ?>
+      Invalid IRP. Try again.
+    <?php } ?>
+  </section>
+  <?php
+    }
+  ?>
+  <?php
+    print_r($_SESSION);
+  ?>
   <h2>Identify Key Objectives</h2>
   <p>Copy the Incident Response Playbook to analyse</p>
-  <p>
-    <textarea name="irp" rows="8" cols="80"></textarea>
-  </p>
-  <button type="button" name="button">Analyse</button>
+  <form class="" action="/inc/curl.inc.php" method="post">
+    <p>
+      <textarea name="irp" rows="8" cols="80" placeholder="Incident Response Playbook goes here">
+        <?php if (strlen($_SESSION["irp"])) echo $_SESSION["irp"]; ?>
+      </textarea>
+    </p>
+    <button type="submit" name="button">Analyse</button>
+  </form>
   <h2>Key Objectives</h2>
   <p>Key objectives identified by LLM are as follows</p>
   <p>
-    <textarea name="objectives" rows="8" cols="80"></textarea>
+    <textarea name="objectives" rows="8" cols="80" placeholder="Incident Response Objectives will appear here">
+      <?php if (strlen($_SESSION["obj"])) echo $_SESSION["obj"]; ?>
+    </textarea>
   </p>
-
-  <a href="/curl.php">Curl</a>
 
 <?php
   include 'inc/footer.inc.php';

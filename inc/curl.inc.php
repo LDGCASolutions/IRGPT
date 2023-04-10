2bdaeb8220b4d8029266c28f7581284bb9f23a09
Charith <charithakalanka@live.com>
2023-04-10 11:35:25 +1000

    Identify completed and missed objectives

--- inc/curl.inc.php
+++ inc/curl.inc.php
@@ -2,63 +2,142 @@
 
 include 'session.inc.php';
 
+function curl($prompt) {
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  $post = [
+    "model" => "text-davinci-003",
+    "prompt" => $prompt,
+    "temperature" => 0,
+    "max_tokens" => 500,
+    "top_p" => 1,
+    "frequency_penalty" => 0,
+    "presence_penalty" => 0
+  ];
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+
+  $headers = array();
+  $headers[] = 'Content-Type: application/json';
+  $headers[] = 'Authorization: Bearer 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';
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $result = curl_exec($ch);
+  if (curl_errno($ch)) {
+      echo 'Error:' . curl_error($ch);
+  }
+  curl_close($ch);
+
+  $result = json_decode($result, true);
+
+  return $result;
+}
+
 $apiKey = "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";
 // print_r($_POST);
 
-if (!strlen($_POST['irp'])) {
-  // No content to post to API
-  header( 'location: /index.php?error=1' );
-  exit();
-}
+// Option 1 : Analyse IRP
+// Option 2 : Analyse responder conversation
 
-$_SESSION["irp"] = $_POST['irp'];
+$prompt = "";
 
-$ch = curl_init();
+if (isset($_GET["opt"]) && $_GET["opt"]==1) {
+  echo "Option 1";
+  if (!strlen($_POST['irp'])) {
+    // No content to post to API
+    header( 'location: /index.php?error=1' );
+    exit();
+  } else {
+    $_SESSION["irp"] = $_POST['irp'];
+    $prompt = "summerise the following playbook into a list of platform independent objectives\n\n".$_POST['irp']."\n\nthe objectives are:";
 
-curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-$post = [
-  "model" => "text-davinci-003",
-  "prompt" => "summerise the following playbook into a list of platform independent objectives\n\n".$_POST['irp']."\n\nthe objectives are:",
-  "temperature" => 0,
-  "max_tokens" => 500,
-  "top_p" => 1,
-  "frequency_penalty" => 0,
-  "presence_penalty" => 0
-];
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+    $result = curl($prompt);
 
-$headers = array();
-$headers[] = 'Content-Type: application/json';
-$headers[] = 'Authorization: Bearer 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';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    if (array_key_exists("error", $result)) {
+      // OpenAI API returned an error
+      echo "API Error<br>";
+      print_r($result);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close($ch);
+    } else {
+      echo "GOOD<br>";
+      $_SESSION["obj"] = trim($result["choices"][0]["text"]);
+    }
 
-$result = json_decode($result, true);
-// print_r($result);
+  }
+} elseif (isset($_GET["opt"]) && $_GET["opt"]==2) {
+  echo "Option 2";
+  if (!strlen($_POST['conv'])) {
+    // No content to post to API
+    header( 'location: /index.php?error=2' );
+    exit();
+  } else {
+    $_SESSION["conv"] = $_POST['conv'];
+    $prompt = "What security actions were performed\n\n".$_POST['conv']."\n\nsteps are:";
 
+    $result = curl($prompt);
 
-if (array_key_exists("error", $result)) {
-  // OpenAI API returned an error
-  echo "API Error<br>";
-  print_r($result);
+    if (array_key_exists("error", $result)) {
+      // OpenAI API returned an error
+      echo "API Error<br>";
+      print_r($result);
 
+    } else {
+      echo "GOOD<br>";
+      $_SESSION["tasks"] = trim($result["choices"][0]["text"]);
+    }
+  }
+
+} elseif (isset($_GET["opt"]) && $_GET["opt"]==3) {
+  // Accept the customised lists
+  $_SESSION['obj'] = $_POST['obj'];
+  $_SESSION['tasks'] = $_POST['tasks'];
+  if (!strlen($_SESSION['obj']) || !strlen($_SESSION['tasks'])) {
+    // No content to post to API
+    header( 'location: /index.php?error=3' );
+    exit();
+  } else {
+    $tasks = preg_replace("/\d+\. /", "-", $_SESSION['tasks']); // Remove numbers from TASK list
+    $prompt = "objectives are\n".$_SESSION['obj']."\ntasks performed are\n".$tasks."\nwhat objectives match the tasks performed?";
+    echo $prompt."<hr>";
+    $result = curl($prompt);
+
+    if (array_key_exists("error", $result)) {
+      // OpenAI API returned an error
+      echo "API Error<br>";
+      print_r($result);
+
+    } else {
+      echo "GOOD<br>";
+      $_SESSION["completed"] = trim($result["choices"][0]["text"]);
+
+      $prompt = "objectives are\n\n".$_SESSION["obj"]."\n\ntasks performed are\n\n".$tasks."\n\nwhat objectives match the tasks performed?\n\n".$_SESSION["completed"]."\n\nwhat objectives were missed?";
+      echo $prompt."<hr>";
+      $result = curl($prompt);
+
+      if (array_key_exists("error", $result)) {
+        // OpenAI API returned an error
+        echo "API Error<br>";
+        print_r($result);
+
+      } else {
+        echo "GOOD<br>";
+        $_SESSION["missed"] = trim($result["choices"][0]["text"]);
+      }
+    }
+  }
 } else {
-  echo "GOOD<br>";
-  // print_r($result["choices"][0]["text"]);
-  $_SESSION["obj"] = $result["choices"][0]["text"];
-
-  // print_r($_SESSION);
-
-  // Reload the home page
-  header( 'location: /index.php' );
+  header( 'location: /index.php?error=0' );
   exit();
 }
+
+// print_r($_SESSION); // For testing
+
+// Reload the home page
+header( 'location: /index.php' );
+exit();
+
+
 
 ?>
--- index.php
+++ index.php
@@ -9,7 +9,13 @@
   ?>
   <section class="error">
     <?php if ($_GET['error']==1) { ?>
-      Invalid IRP. Try again.
+      Empty IRP. Try again.
+    <?php } elseif ($_GET['error']==2) { ?>
+      No evidence. Try again.
+    <?php } elseif ($_GET['error']==3) { ?>
+      Objectives list and/or Task list empty.
+    <?php } else { ?>
+      Unknown Error. Try again.
     <?php } ?>
   </section>
   <?php
@@ -17,7 +23,7 @@
   ?>
   <h2>Identify Key Objectives</h2>
   <p>Copy the Incident Response Playbook to analyse</p>
-  <form class="" action="/inc/curl.inc.php" method="post">
+  <form class="" action="/inc/curl.inc.php?opt=1" method="post">
     <p>
       <textarea name="irp" rows="8" cols="80" placeholder="Incident Response Playbook goes here"><?php if (strlen($_SESSION["irp"])) echo $_SESSION["irp"]; ?></textarea>
     </p>
@@ -29,9 +35,34 @@
   <h2>Key Objectives</h2>
   <p>Key objectives identified by LLM are as follows</p>
   <p>
-    <textarea name="objectives" rows="8" cols="80" placeholder="Incident Response Objectives will appear here"><?php if (strlen($_SESSION["obj"])) echo $_SESSION["obj"]; ?></textarea>
+    <textarea form="missMatch" name="obj" rows="8" cols="80" placeholder="Incident Response Objectives will appear here"><?php if (strlen($_SESSION["obj"])) echo $_SESSION["obj"]; ?></textarea>
   </p>
-
+  <h2>Identify responder achievements</h2>
+  <p>Copy the evidence of the incident response. This could be a transcript of a conversation.</p>
+  <form class="" action="/inc/curl.inc.php?opt=2" method="post">
+    <p>
+      <textarea name="conv" rows="8" cols="80" placeholder="Responder conversation goes here"><?php if (strlen($_SESSION["conv"])) echo $_SESSION["conv"]; ?></textarea>
+    </p>
+    <button type="submit" name="button">Analyse</button>
+  </form>
+  <form class="" action="inc/clear.inc.php" method="post">
+    <button type="submit" name="button">Clear</button>
+  </form>
+  <h2>Key achievements</h2>
+  <p>Key achievements identified by LLM are as follows</p>
+  <p>
+    <textarea form="missMatch" name="tasks" rows="8" cols="80" placeholder="Tasks performed will appear here"><?php if (strlen($_SESSION["tasks"])) echo $_SESSION["tasks"]; ?></textarea>
+  </p>
+  <h2>IRP objectives met by the responder</h2>
+  <form id="missMatch" class="" action="/inc/curl.inc.php?opt=3" method="post">
+    <button type="submit" name="button">Analyse</button>
+  </form>
+  <form class="" action="inc/clear.inc.php" method="post">
+    <button type="submit" name="button">Clear</button>
+  </form>
+  <textarea name="completed" rows="8" cols="80" placeholder="Completed objectives will appear here"><?php if (strlen($_SESSION["completed"])) echo $_SESSION["completed"]; ?></textarea>
+  <h2>IRP objectives missed by the responder</h2>
+  <textarea name="missed" rows="8" cols="80" placeholder="Missed objectives will appear here"><?php if (strlen($_SESSION["missed"])) echo $_SESSION["missed"]; ?></textarea>
 <?php
   include 'inc/footer.inc.php';
 ?>
